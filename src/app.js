e5e2a1708e3da486e161afeb4a505dd7a34bb935
zhangxiaoxi <2054665596@qq.com>
2020-07-23 05:01:41 +0800

    feat: 增加session配置

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+
+const { REDIS_CONF } = require('./conf/db.js')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -18,6 +22,22 @@ app.use(bodyparser({
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
+
+// session
+app.keys = ['ZXX_1996#$'] // 加密秘钥
+app.use(session({
+  key: 'weibo.sid', // cookie name 默认是‘koa.sid’
+  prefix: 'weibo:sess:', // redis key 的前缀，默认是‘koa:sess:’
+  cookie: {
+    path: '/', // 这样配置在网站的所有地方，都可以访问
+    httpOnly: true, // 设置只有后端，才能修改这里的cookie
+    maxAge: 24 * 60 * 60 * 1000 // cookie过期时间,单位是毫秒
+  },
+  ttl: 24 * 60 * 60 * 1000, // redis中的key-value过期时间，单位是毫秒
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  }) // 存储cookie的仓库，我们刚刚引入的koa-redis生成的
+}))
 
 // logger
 app.use(async (ctx, next) => {
